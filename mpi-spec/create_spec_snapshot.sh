99131f80ecf471f256918b6b3a09960ab904f45a
Christian Trott (crtrott) <crtrott@sandia.gov>
2019-01-14 08:30:04 -0700

    Adding Script to generate submission directory for SPEC-MPI
    You should run this out-of-source. Requires python and git.
    It will clone Kokkos and MiniMD, check out a specific version of Kokkos
    and create a new directory with a combined miniMD/mpi-spec and kokkos
    sparse snapshot.

---
+++ mpi-spec/create_spec_snapshot.sh
@@ -0,0 +1,14 @@
+mkdir minimd-submission
+cd minimd-submission
+git clone https://github.com/kokkos/kokkos
+cd kokkos
+git checkout 2.7.24
+cd ..
+mkdir submission
+./kokkos/scripts/snapshot.py --small /Users/crtrott/Kokkos/kokkos submission
+rm -rf submission/kokkos/core/src/eti/*/*.cpp
+
+git clone https://github.com/mantevo/miniMD
+cp -r miniMD/mpi-spec/* submission
+
+
